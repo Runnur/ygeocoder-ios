b4fa5766b2efe9c498c96d167774b4d33967f313
Alexey Storozhev <dark.storoj@gmail.com>
2013-03-10 20:23:51 +0400

    files added

---
+++ ygeocoder/YGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YGAppDelegate.h
+//  ygeocoder
+//
+//  Created by Alexey Storozhev on 03/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ygeocoder/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  ygeocoder
+//
+//  Created by Alexey Storozhev on 03/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YGAppDelegate class]));
+    }
+
+}
